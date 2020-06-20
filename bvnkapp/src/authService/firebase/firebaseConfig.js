4aeb54182ca09bbbc7e34c4d320487b3d1625bfa
Your Name <william.fleckenstein@gmail.com>
2020-06-19 21:29:57 -0500

    firebase config added and signup  action created

---
+++ bvnkapp/src/authService/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+export const config = {
+  apiKey: "AIzaSyAlqtEnMYXpbPrdtEJQ_tnEneK7E_FVHYg",
+  authDomain: "bvnk-af268.firebaseapp.com",
+  databaseURL: "https://bvnk-af268.firebaseio.com",
+  projectId: "bvnk-af268",
+  storageBucket: "bvnk-af268.appspot.com",
+  messagingSenderId: "823298933702",
+  appId: "1:823298933702:web:aa558097b02bb0166628c7",
+  measurementId: "G-V5FKENVWTB",
+};
+// Initialize Firebase
---
+++ bvnkapp/src/redux/auth/registerActions.js
@@ -0,0 +1,54 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import { history } from "../../history";
+
+import { config } from "../../authService/firebase/firebaseConfig";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+let firebaseAuth = firebase.auth();
+
+export const signupWithFirebase = (email, password, name) => {
+  return (dispatch) => {
+    let userEmail = null,
+      loggedIn = false;
+
+    firebaseAuth
+      .createUserWithEmailAndPassword(email, password, name)
+      .then((result) => {
+        firebaseAuth.onAuthStateChanged((user) => {
+          console.log("user", user);
+          result.user.updateProfile({
+            displayName: name,
+          });
+          if (user) {
+            userEmail = user.email;
+            let userName = user.displayName;
+            loggedIn = true;
+            dispatch({
+              type: "SIGNUP_WITH_EMAIL",
+              payload: {
+                email: userEmail,
+                name,
+                isSignedIn: loggedIn,
+              },
+            });
+            dispatch({
+              type: "LOGIN_WITH_EMAIL",
+              payload: {
+                email: userEmail,
+                name,
+              },
+            });
+          }
+        });
+        history.push("/");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+};
